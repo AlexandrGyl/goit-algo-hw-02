19665040edf1f139546b6e8438adb91538919021
Alexander <defakto.gylop@gmail.com>
2025-01-09 22:20:14 +0200

    add STOP the program

--- Task_1/request.py
+++ Task_1/request.py
@@ -6,17 +6,27 @@ import queue
 import random
 import time
 
+k = int(input("Веедіть кількість заявок якібудуть сьогодні оброблено: "))
 request_queue = queue.Queue()
+unique_request = set()
 
-def generate_requeste():
-    request_id = random.randint(1, 100)
-    print(f"Створено заявку із ID: {request_id}")
-    request_queue.put(request_id)
+def generate_request():
+    while True:
+        request_id = random.randint(1, k)
+        if request_id not in unique_request:
+            unique_request.add(request_id)
+            print(f"Створено заявку із ID: {request_id}")
+            request_queue.put(request_id)
+            break
+        if len(unique_request) == k:
+            print("Всі заявки на сьогодні вичерпано")
+            return False
+    return True
 
 def process_request():
     if not request_queue.empty():
         request_id = request_queue.get()
-        print(f"Ообробляється заявка з ID: {request_id}")
+        print(f"Обробляється заявка з ID: {request_id}")
 
         time.sleep(random.uniform(0.5, 2.0))
         print (f"Завка з ID: {request_id} оброблено")
@@ -24,10 +34,17 @@ def process_request():
         print ("Черга пуста")
 
 def main():
-    while True:
-        generate_requeste()
-        process_request()
-        time.sleep(1)
+    count_request = 0
+    while count_request < k:
+        if not generate_request():
+            break
+        if not process_request():
+            count_request += 1
+        time.sleep(1)  # Затримка між ітераціями
+
+    print("Всі заявки на сьогодні вичерпано.")
+
+                 
 
 if __name__ == "__main__":
     main()
